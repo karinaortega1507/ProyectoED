a2c10a3cc3f9a8e578fd0de7df8f8db73f3b8c4a
Yiam_Rodriguez_Delgado <yorodrig@espol.edu.ec>
2019-11-30 17:52:50 -0500

    Cambios en PaneJuego

--- ProyectoParcia1T2019/src/proyectoparcia1t2019/PaneJuego.java
+++ ProyectoParcia1T2019/src/proyectoparcia1t2019/PaneJuego.java
@@ -5,6 +5,9 @@
  */
 package proyectoparcia1t2019;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -40,6 +43,18 @@ public class PaneJuego {
     private Pane parteInferior;
     private VBox cajonOpciones;
     private HBox ordenarBotones;
+    
+    
+    //Opciones menu
+    public Slider Guerreros;
+    public ComboBox<String> elegirDireccion;
+    public TextField elegirIndice;
+    public Label l;
+    
+    
+    
+    
+    
     public PaneJuego() {
 
         areaJuego = new BorderPane();
@@ -50,48 +65,8 @@ public class PaneJuego {
         imageView.setImage(image);
         imageView.setFitHeight(650);
         imageView.setFitWidth(800);
-        //-------------------------------------------------------------------------
-        //CREANDO OPCIONES: TODAS LAS OPCIONES ESTÁN EN UN VBOX
-        //1)NUMERO DE GUERREROS EN LA SIMULACIÓN/SLIDE
-        final Slider Guerreros = new Slider(8, 30, 8); //Creo el Slider
-        Guerreros.setShowTickMarks(false); Guerreros.setShowTickLabels(true);//No se muestran los milimetros y se muestran las etiquetas de numeros enteros
-        Guerreros.setMajorTickUnit(1); Guerreros.setBlockIncrement(1);//Se establece las etiquetas de cuanto en cuanto serán visibles y el incremento que va a tener por cada salto en teclado
-        Label GuerreroLabel=new Label("Escoja el número de soldados para hacer la simulación"); 
-        Label l=new Label("");//Establecida para mostrar los numeros cambiantes en pantalla
-        //Método para mostrar en pantalla el número
-        Guerreros.valueProperty().addListener( 
-            new ChangeListener<Number>() { 
-            public void changed(ObservableValue <? extends Number >  
-                      observable, Number oldValue, Number newValue) 
-            { 
-                l.setText("value: " + newValue.intValue()); //selecciona solo números enteros
-            } 
-        }); 
-        //2)ORIENTACIÓN DE LA SIMULACIÓN/COMBOBOX
-        ObservableList<String> direccion = FXCollections.observableArrayList();
-        direccion.addAll("Derecha", "Izquierda");
-        Label direccionLabel=new Label("Escoja la orientación del juego para hacer la simulación"); 
-        ComboBox<String> elegirDireccion = new ComboBox<>(direccion);//Se elegirá el índice que comienza a eliminar
-        //cajonOpciones.getChildren().add(elegirDireccion);
-       
-       //3) Elegir quien inicia la matanza/TEXTFIELD
-       TextField elegirIndice=new TextField();
-       Label indiceLabel=new Label("Ingrese un número, representará el soldado que comenzará la simulación"); 
-       elegirIndice.setMinWidth(50);
-       elegirIndice.setMaxWidth(50);
-       
-       //4)Botón Simulación/BUTTON
-        Button botonSimulacion=new Button("Comenzar Simulación");
-        Button botonStop=new Button("Terminar Simulación");
-        
-        
-        //PONIENDO EN EL VBOX LAS OPCIONES 
-        cajonOpciones.setPadding(new Insets(20)); //Separa el VBox del borde de la ventana 20 pixeles
-        cajonOpciones.setSpacing(10); //Espacio entre cada VBox
-        cajonOpciones.getChildren().addAll(GuerreroLabel, Guerreros, l,direccionLabel,elegirDireccion,indiceLabel,elegirIndice,botonSimulacion,botonStop); 
-//------------------------------------------------------------------------------------
-        //Comenzamos a poner en el root los elementos de la simulación
-        areaJuego.setRight(cajonOpciones);
+ 
+
         Group root = new Group();
         Circle circulo = new Circle();
         Circle circulo2,circulo3;
@@ -107,6 +82,15 @@ public class PaneJuego {
         centro.getChildren().addAll(imageView, root);
        
         areaJuego.setCenter(centro);
+        panelOpciones();
+        
+        
+        
+         ////Como obtener el valor que escogimos del menu opciones
+        String dir= elegirDireccion.getSelectionModel().getSelectedItem().toString();//Donde se encontraran contenidas la categorias de ingreso
+        String indice =elegirIndice.getText();
+        String numeroParticipantes= l.getText();
+        
     }
     public void cargarPersonas(Circle c,Group root) {
         // Pedir el numero de personas o recuperarlo del slider
@@ -134,7 +118,124 @@ public class PaneJuego {
         return areaJuego;
 
     }
-     }
+    public void panelOpciones(){
+        
+                //-------------------------------------------------------------------------
+        //CREANDO OPCIONES: TODAS LAS OPCIONES ESTÁN EN UN VBOX
+        //1)NUMERO DE GUERREROS EN LA SIMULACIÓN/SLIDE
+        Guerreros = new Slider(8, 30, 8); //Creo el Slider
+        Guerreros.setShowTickMarks(false); Guerreros.setShowTickLabels(true);//No se muestran los milimetros y se muestran las etiquetas de numeros enteros
+        Guerreros.setMajorTickUnit(1); Guerreros.setBlockIncrement(1);//Se establece las etiquetas de cuanto en cuanto serán visibles y el incremento que va a tener por cada salto en teclado
+        Label GuerreroLabel=new Label("Escoja el número de soldados para hacer la simulación"); 
+        l=new Label("");//Establecida para mostrar los numeros cambiantes en pantalla
+        //Método para mostrar en pantalla el número
+        Guerreros.valueProperty().addListener( 
+            new ChangeListener<Number>() { 
+            public void changed(ObservableValue <? extends Number >  
+                      observable, Number oldValue, Number newValue) 
+            { 
+                l.setText("value: " + newValue.intValue()); //selecciona solo números enteros
+            } 
+        }); 
+        //2)ORIENTACIÓN DE LA SIMULACIÓN/COMBOBOX
+        ObservableList<String> direccion = FXCollections.observableArrayList();
+        direccion.addAll("Derecha", "Izquierda");
+        Label direccionLabel=new Label("Escoja la orientación del juego para hacer la simulación"); 
+        elegirDireccion = new ComboBox<>(direccion);//Se elegirá el índice que comienza a eliminar
+        //cajonOpciones.getChildren().add(elegirDireccion);
+        elegirDireccion.getSelectionModel().select(0);
+
+        
+        
+        
+       //3) Elegir quien inicia la matanza/TEXTFIELD
+       elegirIndice=new TextField();
+       Label indiceLabel=new Label("Ingrese un número, representará el soldado que comenzará la simulación"); 
+       elegirIndice.setMinWidth(50);
+       elegirIndice.setMaxWidth(50);
+       
+       //4)Botón Simulación/BUTTON
+        Button botonSimulacion=new Button("Comenzar Simulación");
+        Button botonStop=new Button("Terminar Simulación");
+        
+        
+        //PONIENDO EN EL VBOX LAS OPCIONES 
+        cajonOpciones.setPadding(new Insets(20)); //Separa el VBox del borde de la ventana 20 pixeles
+        cajonOpciones.setSpacing(10); //Espacio entre cada VBox
+        cajonOpciones.getChildren().addAll(GuerreroLabel, Guerreros, l,direccionLabel,elegirDireccion,indiceLabel,elegirIndice,botonSimulacion,botonStop);
+        //------------------------------------------------------------------------------------
+        //Comenzamos a poner en el root los elementos de la simulación
+        areaJuego.setRight(cajonOpciones);
+    }
+   
+    
+    
+     
+    
+    
+    
+    
+      
+      
+      
+      
+      
+    class HiloCambiarColor implements Runnable {
+        Persona c;
+       
+
+        public HiloCambiarColor(Persona c) {
+            this.c = c;
+         
+        }
+
+        public void run() {
+
+            try {
+                
+                Thread.sleep(1000);
+                Platform.runLater(()->{
+                    c.getCirculo().setFill(Color.BLACK);
+                });
+
+            } catch (InterruptedException ex) {
+                Logger.getLogger(PaneJuego.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+
+    }
+    class HiloTenerEspada implements Runnable {
+        Persona c;
+       
+
+        public HiloTenerEspada(Persona c) {
+            this.c = c;
+         
+        }
+
+        public void run() {
+
+            try {
+                
+                Thread.sleep(1000);
+                Platform.runLater(()->{
+                    
+                    
+                    
+                    //////////Colocar la espada en el jugador
+                    
+                  ////  c.getCirculo().setFill(Color.BLACK);
+                });
+
+            } catch (InterruptedException ex) {
+                Logger.getLogger(PaneJuego.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+
+    }
+}
     
           
         
